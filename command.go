10b26eb9594f466d552e2027efa23c0d809cb417
Simon <baeumer@pm.me>
2019-10-02 10:59:39 +0200

    Fix race condition

--- command.go
+++ command.go
@@ -174,7 +174,6 @@ func (c *Command) Execute() error {
 		c.exitCode = 0
 	case <-timeoutChan:
 		quit <- true
-		close(done)
 		if err := cmd.Process.Kill(); err != nil {
 			return fmt.Errorf("Timeout occurred and can not kill process with pid %v", cmd.Process.Pid)
 		}
