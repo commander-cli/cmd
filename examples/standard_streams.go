b7d83e775a89605de6cc3100693577e0a09778aa
Simon <baeumer@pm.me>
2019-10-24 12:13:32 +0200

    Add example documentation

--- examples/standard_streams.go
+++ examples/standard_streams.go
@@ -2,6 +2,7 @@ package examples
 
 import "github.com/SimonBaeumer/cmd"
 
+// CreateNewCommandWithStandardStream create new standard stream example
 func CreateNewCommandWithStandardStream() {
 	c := cmd.NewCommand("echo hello; sleep 1; echo another;", cmd.WithStandardStreams)
 	c.Execute()
