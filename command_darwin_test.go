91e15c8c0544aa2e3da700abbf79d38c68d9401a
dylanhitt <dylan.hitt1@gmail.com>
2022-08-29 11:30:34 -0400

    Migrate WithCustomerBaseCommand test to linux test suite for test coverage purposes

--- command_darwin_test.go
+++ command_darwin_test.go
@@ -1,25 +1,11 @@
 package cmd
 
 import (
-	"os/exec"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestCommand_WithCustomBaseCommand(t *testing.T) {
-	cmd := NewCommand(
-		"echo $0",
-		WithCustomBaseCommand(exec.Command("/bin/bash", "-c")),
-	)
-
-	err := cmd.Execute()
-	assert.Nil(t, err)
-	// on darwin we use /bin/sh by default test if we're using bash
-	assert.NotEqual(t, "/bin/sh\n", cmd.Stdout())
-	assert.Equal(t, "/bin/bash\n", cmd.Stdout())
-}
 
 func TestCommand_ExecuteStderr(t *testing.T) {
 	cmd := NewCommand(">&2 echo hello")
