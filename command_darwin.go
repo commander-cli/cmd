4b249528d0c0305a57cdaa2299b5d50834c0d54c
dylanhitt <dylan.hitt1@gmail.com>
2022-11-24 13:01:22 -0500

    Add WithUser docs

--- command_darwin.go
+++ command_darwin.go
@@ -10,6 +10,14 @@ func createBaseCommand(c *Command) *exec.Cmd {
 	return cmd
 }
 
+// WithUser allows the command to be run as a different
+// user.
+//
+// Example:
+//
+//	cred := syscall.Credential{Uid: 1000, Gid: 1000}
+//	c := NewCommand("echo hello", cred)
+//	c.Execute()
 func WithUser(credential syscall.Credential) func(c *Command) {
 	return func(c *Command) {
 		c.baseCommand.SysProcAttr = &syscall.SysProcAttr{
